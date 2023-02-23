a7697884aa17e4a2c7c8197c02f4b2fa6bf74a77
MooTheCau <126118872+MooTheCau@users.noreply.github.com>
2023-02-22 21:51:39 -0500

    Log private messages

--- Modules/chat_write_to_logfile.py
+++ Modules/chat_write_to_logfile.py
@@ -6,11 +6,13 @@ HasHelpInfo = False
 
 def handler(msg_pipe):
     msg = msg_pipe.get()
-    logdir = pathlib.Path(pathlib.Path.cwd() / "room_logs")
+    logdir = pathlib.Path(pathlib.Path.cwd() / "logs")
     if not logdir.exists():
         logdir.mkdir(exist_ok=True)
+        
+    filename = msg.json["channel"] if "channel" in msg.json.keys() else msg.json["character"]
            
-    open("room_logs/%s.log" % msg.json["channel"], "a", encoding="utf-8").write("[%s] %s: %s\n" % (str(datetime.datetime.now()), msg.json["character"], msg.json["message"]))
+    open("logs/%s.log" % filename, "a", encoding="utf-8").write("[%s] %s: %s\n" % (str(datetime.datetime.now()), msg.json["character"], msg.json["message"]))
     
 def predicate(msg):
-    return msg.code == "MSG"
+    return msg.code == "MSG" or msg.code == "PRI"
